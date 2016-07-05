d06fa84adc64bd5c32d24eb661312b3b916da716
Nicholas Corgan <n.corgan@gmail.com>
2016-07-05 06:46:25 -0700

    travis: install new prerequisites

--- travis/install-deps.sh
+++ travis/install-deps.sh
@@ -7,6 +7,11 @@
 #
 
 sudo apt-get -y update
-sudo apt-get -y install build-essential git cmake swig gcc \
-                        g++ doxygen mono-complete default-jre \
-                        libpython-dev libboost-dev python-ply \
+sudo apt-get -y install build-essential git cmake swig gcc g++ \
+                        libboost-dev doxygen mono-complete default-jre \
+                        libpython-dev libpython3-dev python-ply python3-ply \
+			python-pip python3-pip
+sudo pip2 install CppHeaderParser
+sudo pip3 install CppHeaderParser
+
+dpkg --list | grep swig
